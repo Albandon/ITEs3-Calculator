d3ca43c4fc8e6d8e4276c3b2b9da3469b45ddebc
Alban <albanstudios@gmail.com>
2024-08-01 18:23:37 +0200

    Now works for complementary bases. Added Multiplication (complementary not tested)

--- Integers.cs
+++ Integers.cs
@@ -1,16 +1,12 @@
-using System.Collections.Immutable;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.JavaScript;
 
 namespace CalculatorApp;
 public class Integers {
     private readonly List<int> _number = [];
-    private readonly int _base;
-    private readonly bool _isComplement = false;
+    private readonly bool _isComplement;
 
     public Integers() {
         _number.Add(0);
-        _base = 10;
+        GetBase = 10;
     }
 
     /// <summary>
@@ -18,90 +14,125 @@ public class Integers {
     /// </summary>
     /// <param name="number">decimal representation</param>
     /// <param name="toBase">desired base to convert to</param>
-    public Integers(int number, int toBase) {
+    public Integers(long number, int toBase = 10) {
         _isComplement = number < 0; //if negative then complement base
         number = Math.Abs(number);
         while (number > 0) {
-            _number.Add(number % toBase);
+            _number.Add((int)(number % toBase));
             number /= toBase;
         }
-        _base = toBase;
-        if (_base == 10) return;
-        _number.Add(_isComplement? toBase - 1: 0);
+        GetBase = toBase;
+        _number.Add(0);
         if (_isComplement) _number = Neg()._number;
-        PadBinNumber();
+        // _number.Add(_isComplement? toBase - 1: 0);
+        // if (_base == 10) return;
+        // PadBinNumber();
     }
 
     public Integers(string number, int inBase, bool isComplement = false) {
         // validate if it's a number to max base 16
         // add interpretation of negative numbers
+        // if (number.Last() >= inBase / 2) isComplement = true;   
+        var dec = Convert.ToInt32(number, inBase);
         _isComplement = isComplement;
-        _base = inBase;
-        for (int i = number.Length - 1; i >= 0; i--) {
-            _number.Add(number[i] % inBase);
+        while (dec > 0) {
+            _number.Add(dec % inBase);
+            dec /= inBase;
         }
-        // if (_IsComplement) _Number = Neg()._Number;
+        GetBase = inBase; 
+        // if (_isComplement) _number = Neg()._number;
     }
 
     public Integers(IEnumerable<int> number, int toBase, bool isComplement = false) {
         _isComplement = isComplement;
         _number = number.ToList();
-        _base = toBase;
+        GetBase = toBase;
         // if (isComplement && _Base != 2) _Number = Neg()._Number;
-        PadBinNumber();
+        // PadBinNumber();
     }
     
     public static Integers operator+ (Integers a, Integers b) { // introduce custom Exception
-        if (a._base != b._base) throw new ArgumentException("Different Bases");
+        if (a.GetBase != b.GetBase) throw new ArgumentException("Different Bases");
         var memory = 0;
         var product = new List<int>();
-        var Base = a._base;
+        var numBase = a.GetBase;
         for (int i = 0; i < Math.Max(a._number.Count,b._number.Count); i++) {
             var result = a._number.ElementAtOrDefault(i) + b._number.ElementAtOrDefault(i) + memory;
-            product.Add(result % Base);
-            memory = result / Base;
+            product.Add(result % numBase);
+            memory = result / numBase;
         }
-        return new Integers(product,Base);
+        // var overflow = Mod(memory, numBase);
+        // if (overflow > 0) product.Add(overflow);
+        return new Integers(product,numBase);
     }
 
-    public static Integers operator -(Integers a, Integers b) { //problem with how we define default for negatives -> we don't xD
-        // possible solution -> separate class for number containment with get method which would give you Element At or Default 
-        // BASICALLY Class (or Record tbh) with List<int> Number and a few methods
-        if (a._base != b._base) throw new ArgumentException("Different Bases");
+    public static Integers operator -(Integers a, Integers b) {
+        if (a.GetBase != b.GetBase) throw new ArgumentException("Different Bases");
         var memory = 0;
         var product = new List<int>();
-        var Base = a._base;
-        var negative = a._isComplement && b._isComplement;
+        var numBase = a.GetBase;
+        var negative = false;
         for (int i = 0; i < Math.Max(a.GetNumberLength, b.GetNumberLength); i++) {
             var result =  a._number.ElementAtOrDefault(i) - b._number.ElementAtOrDefault(i) - memory;
             memory = result < 0 ? 1 : 0;
-            product.Add(Mod(result,Base));
+            product.Add(Mod(result,numBase));
         }
         if (memory == 1) negative = true;
         while (product[^1] == 0 && product.Count>1) {
             product = product[..^1]; //delete last element == pop xD
         }
-        return new Integers(product, Base, negative);
+        return new Integers(product, numBase, negative);
+    }
+
+    public static Integers operator *(Integers a, Integers b) { // works
+        if (a.GetBase != b.GetBase) throw new ArgumentException("Different Bases");
+        var memory = 0;
+        var product = new int[a.GetNumberLength+b.GetNumberLength - 1];
+        var negative = a._isComplement != b._isComplement;
+        for (int i = 0; i < b.GetNumberLength; i++) {
+            for (int j = 0; j < a.GetNumberLength; j++) {
+                var result = product[i + j] + a._number.ElementAtOrDefault(j) 
+                             * b._number.ElementAtOrDefault(i) + memory;
+                memory = result / a.GetBase;
+                product[i+j] = result % a.GetBase;
+            }
+        }
+        while (product[^1]==0 && product.Length>1) {
+            product = product[..^1];
+        }
+        return new Integers(product, a.GetBase, negative);
     }
     public override string ToString() {
         char[] numSymbol = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
         var temp = _number;
         var str = "";
-        if (_base == 10 && _isComplement) {
+        if (GetBase == 10 && _isComplement) {
             temp = Neg()._number;
             str = "-";
         }
+        if (GetBase != 10) str += _isComplement ? numSymbol[GetBase - 1] : numSymbol[0];
+        str += PadString(str);
+        // PadList(temp);
         for (int i = temp.Count - 1; i >= 0; i--) {
-            if (_base == 2 && (i + 1) % 4 == 0 && (i + 1) != temp.Count) str += " "; 
+            if (GetBase == 2 && (i + 1) % 4 == 0 ) str += " "; 
             var curSymbol = temp[i];
             str += numSymbol[curSymbol];
-
         }
         return str;
     }
-    
-    public Integers Neg() =>
+    private Integers Neg() =>
         MaxValue(GetBase, GetNumberLength) - this + new Integers(1,GetBase);
+
+    public Integers ConvertTo(int toBase) { // conversion from negative isn't ideal
+        long dec = 0;
+        var neg = _isComplement?Neg()._number : _number;
+        for (int i = 0; i < neg.Count; i++) {
+            dec += neg[i] * (int)Math.Pow(GetBase, i);
+        }
+
+        if (_isComplement) dec *= -1;
+        return new Integers(dec, toBase); 
+    }
     
     private static Integers MaxValue(int @base, int length) {
         var temp = new int[length];
@@ -110,24 +141,29 @@ public class Integers {
     }
 
     public static Integers operator ^(Integers a, Integers b) {
-        if (a._base != b._base) throw new ArgumentException("Different Base");
-        var count = Math.Max(a._number.Count, b._number.Count);
+        if (a.GetBase != b.GetBase) throw new ArgumentException("Different Base");
+        var cA = a.ConvertTo(2);
+        var cB = b.ConvertTo(2);
+        var count = Math.Max(cA._number.Count, cB._number.Count);
         var temp = new int [count];
         for (int i = 0; i < count; i++) {
-            temp[i] = a._number.ElementAtOrDefault(i) ^ b._number.ElementAtOrDefault(i);
+            temp[i] = cA._number.ElementAtOrDefault(i) ^ cB._number.ElementAtOrDefault(i);
         }
-        return new Integers(temp, a._base);
+        return new Integers(temp, cA.GetBase).ConvertTo(a.GetBase);
     }
     public int GetNumberLength => _number.Count;
-    public int GetBase => _base;
-
-    private void PadBinNumber() {
-        if (_base != 2) return;
-        var count = _number.Count % 4;
-        if (count == 0) return;
+    public int GetBase { get; }
+    
+    private string PadString(string s) {
+        char[] numSymbol = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+        var str = "";
+        if (GetBase != 2) return "";
+        var count = s.Length % 4;
+        if (count == 0) return "";
         for (int i = 0; i < 4 - count; i++) {
-            _number.Add(_isComplement? _base - 1: 0);
+            str += _isComplement ? numSymbol[GetBase - 1] : numSymbol[0];
         }
+        return str;
     }
 
     private static int Mod(int a, int b) =>
--- Program.cs
+++ Program.cs
@@ -3,16 +3,23 @@
 using CalculatorApp;
 
 Console.WriteLine("Hello, World!");
-var a = new Integers(10, 10);
-var b = new Integers(11, 10);
-Console.WriteLine(a);
-Console.WriteLine(b);
-var c = a - b;
+
+// var a = new Integers(10, 2);
+// var b = new Integers(11, 2);
 // Console.WriteLine(a);
 // Console.WriteLine(b);
-// c.Neg();
-Console.WriteLine(c);
-Console.WriteLine(c + b);
-// Console.WriteLine(new Integers(10, 2) - new Integers(9,2));
-// Console.WriteLine((new Integers("1011",2)+new Integers("0011",2)).ToString());
-// Console.WriteLine((new Integers(211,10 ) + new Integers(49,10)).ToString());
+// var c = a - b;
+// Console.WriteLine(c.ConvertTo(16)); //conversion from u2 to dec and u8 == bad
+// Console.WriteLine(c + b);
+// var d = new Integers("A109", 16);
+// Console.WriteLine(d);
+// Console.WriteLine(d.ConvertTo(10));
+// Console.WriteLine(d.ConvertTo(8));
+// Console.WriteLine(d.ConvertTo(2));
+// // Console.WriteLine(new Integers(-121311,16));
+// var v = new Integers(-19, 10);
+// Console.WriteLine(v);
+Console.WriteLine(new Integers(12512)*new Integers(3));
+Console.WriteLine(new Integers(12512,16)*new Integers(3,16));
+Console.WriteLine(new Integers(12512,8)*new Integers(3,8));
+Console.WriteLine(new Integers(12512,2)*new Integers(3,2));
